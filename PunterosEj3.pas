d6bb05f574939ca1c88b7fcdac4a0a9a207c7b35
BlackMouth3900 <amolerom.alumnos@mercedariastrescantos.es>
2025-01-30 12:42:07 +0100

    30/01/2025

--- PunterosEj3.pas
+++ PunterosEj3.pas
@@ -1,13 +1,16 @@
 program PunterosEj3;
 type
-    { Declara un tipo de registro llamado “nodo” con dos campos: un entero y un puntero a “nodo” }
-
+    { Declara un tipo de registro llamado â€œnodoâ€� con dos campos: un entero y un puntero a â€œnodoâ€� }
+    TREC= RECORD
+    	ENT: INTEGER;
+    	PRT: ^INTEGER;
+	END;
 
 var
-    { Declara una variable de tipo puntero a nodo }
+    REC: TREC;
 
 begin
-    { Crea un nodo en memoria dinámica (heap) }
+    { Crea un nodo en memoria dinÃ¡mica (heap) }
 
 
     { Dale valor 100 al campo entero de este nodo }
@@ -19,11 +22,11 @@ begin
     { Pon ahora el puntero del nodo a apuntar al propio nodo }
 
 
-    { En este punto hay dos instrucciones alternativas para liberar la memoria dinámica creada: }
+    { En este punto hay dos instrucciones alternativas para liberar la memoria dinÃ¡mica creada: }
     { Alternativa 1: Liberar el nodo directamente }
 
 
-    { Alternativa 2: Liberar el nodo a través del campo puntero }
+    { Alternativa 2: Liberar el nodo a travÃ©s del campo puntero }
     { Dispose(punteroNodo^.siguiente); }
 
     { Termina }
