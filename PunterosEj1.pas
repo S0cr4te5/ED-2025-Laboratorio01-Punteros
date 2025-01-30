c2d9c7f96669569201365fc356c7dbecc391a9b6
BlackMouth3900 <amolerom.alumnos@mercedariastrescantos.es>
2025-01-30 12:37:51 +0100

    30/01/2025

--- PunterosEj1.pas
+++ PunterosEj1.pas
@@ -1,42 +1,43 @@
-{ 
-    Programa que demuestra el uso de punteros en Pascal 
-}
-
 program PunterosEj1;
 
-uses
-    ctr;
-
 var
-    { Declara variable entera (x) }
-    { Declara una variable de tipo puntero a entero (p_ent) }
+    x: INTEGER;
+    P_ent: ^integer;
 
 begin
-
+	{ Declara variable entera (x) }
+    { Declara una variable de tipo puntero a entero (p_ent) }
     { Da el valor 100 a x }
-
-    { Crea un entero dinámicamente con p_ent y dale el valor que tiene actualmente x }
-
+	x:=100;
+    { Crea un entero din�micamente con p_ent y dale el valor que tiene actualmente x }
+    new(P_ent);
+    p_ent^:=x;
     { Imprime por pantalla el valor contenido en el entero al que apunta p_ent }
-
+	WRITELN(p_ent^);
     { Crea con new un nuevo entero dinámicamente }
-
+	new(P_ent);
     { Imprime por pantalla el valor contenido en el entero al que apunta p_ent }
-
+	WRITELN(p_ent^);
     { Pon el puntero p_ent a apuntar a x }
-
+	dispose(p_ent);
+	P_ent:=@x;
     { Súmale 100 al entero apuntado por el puntero p_ent }
-
+	P_ent^:=P_ent^+100;
     { Imprime por pantalla el valor de x y también del entero al que apunta p_ent }
-
+	WRITELN(x);
+	WRITELN(p_ent^);
     { Pon el puntero p_ent a NIL }
-
+	P_ent:=NIL;
     { Libera la memoria asociada al nuevo entero }
-
+	dispose(p_ent);
     { Suma 100 a x pero sin usar x en la operación de suma (solo usando p_ent) }
-
+	P_ent:=@x;
+	P_ent^:=P_ent^+100;
+	P_ent:=NIL;
     { Muestra por pantalla que son iguales }
-
+	WRITELN(p_ent^);
     { Libera toda la memoria asociada a p_ent y termina }
+	dispose(p_ent);
+	READLN();
 
 end.
--- PunterosEj2.pas
+++ PunterosEj2.pas
@@ -1,30 +1,39 @@
 program PunterosEj2;
-
-uses
-    SysUtils;
-
 var
-
+Arr: Array [0..2] of INTEGER;
+i: INTEGER;
+ptr: ^INTEGER;
 
 begin
-    { Crea un array de 3 enteros V e inicialízalo con números aleatorios. }
-
+    { Crea un array de 3 enteros V e inicialÃ­zalo con nÃºmeros aleatorios. }
+    RANDOMIZE;
+	FOR i:= 0 to 2 do
+		Arr[i]:= random(100);
 
     { Muestra sus valores por pantalla }
+	FOR i:= 0 to 2 do
+		WRITELN(Arr[i]);
 
-
-    { Declara un puntero a entero p y ponlo a apuntar a la primera posición del array }
-
+    { Declara un puntero a entero p y ponlo a apuntar a la primera posiciÃ³n del array }
+	ptr:=@Arr[0];
 
     { Pon el valor 100 en el entero apuntado por p }
+	ptr^:=100;
 
-
-    { Muestra los valores del array “V” por pantalla }
-
+    { Muestra los valores del array â€œVâ€� por pantalla }
+	FOR i:= 0 to 2 do
+		WRITELN(Arr[i]);
 
     { Recorre con p todas las posiciones del array para ponerlas todas a cero }
+	FOR i:= 0 to 2 do
+		BEGIN
+		ptr:=@Arr[i];
+		ptr^:=0;
+		END;
 
-
-    { Muestra los valores del array “V” por pantalla }
-
+    { Muestra los valores del array â€œVâ€� por pantalla }
+	FOR i:= 0 to 2 do
+		WRITELN(Arr[i]);
+	dispose(ptr);
+		READLN;
 end.
